a4f872a783b1aeafb0e25415a083dafad7fc2971
xiaohuajiao123 <421228736@qq.com>
2014-01-20 11:58:34 +0800

    add enterprise_session_name to config.php

--- izhiqi/Conf/Home/config.php
+++ izhiqi/Conf/Home/config.php
@@ -5,6 +5,8 @@ $config = array(
     'LOG_LEVEL' => 'EMERG, ALERT, CRIT, ERR',
 
     'USER_SESSION_NAME' => 'user_session_name',
+    'ENTERPRISE_USER_SESSION_NAME' => 'enterprise_user_session_name',
+
     'URL_CASE_INSENSITIVE' => true,
 
     'APP_AUTOLOAD_PATH' => '@.Util.Home',
