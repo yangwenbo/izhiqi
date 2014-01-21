709b7f21d7de9e3ba210d6a169a666a83dd6cae5
xiaohuajiao123 <421228736@qq.com>
2014-01-21 22:45:59 +0800

    edit css

--- Public/js/Validform_v5.3.2.js
+++ Public/js/Validform_v5.3.2.js
@@ -239,12 +239,12 @@
 			curform.find("[datatype]").each(function(){
 				if(tiptype==2){
 					if($(this).parent().next().find(".Validform_checktip").length==0){
-						$(this).parent().next().append("<span class='Validform_checktip' />");
+						$(this).parent().next().append("<p class='Validform_checktip' />");
 						$(this).siblings(".Validform_checktip").remove();
 					}
 				}else if(tiptype==3 || tiptype==4){
 					if($(this).siblings(".Validform_checktip").length==0){
-						$(this).parent().append("<span class='Validform_checktip' />");
+						$(this).parent().append("<p class='Validform_checktip' />");
 						$(this).parent().next().find(".Validform_checktip").remove();
 					}
 				}
