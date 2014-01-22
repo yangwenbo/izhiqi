71ecb76b8129544f191a495b3a5e82d8566eef8d
cs-dong <xudong1991@gmail.com>
2014-01-22 18:11:20 +0800

    add admin

--- izhiqi/Conf/Admin/config.php
+++ izhiqi/Conf/Admin/config.php
@@ -1,7 +1,13 @@
 <?php
-$config = array(
+return $config = array(
     'DEFAULT_FILTER' => 'htmlspecialchars',
     'LOG_RECORD' => true,
     'LOG_LEVEL' => 'EMERG, ALERT, CRIT, ERR',
+	
+	'TMPL_PARSE_STRING' => array(
+		'__JSURL__'		=> WEBSITE . 'Public/js',
+		'__CSSURL__'	=> WEBSITE . 'Public/css',
+		'__IMGURL__'	=> WEBSITE . 'Public/images',
+	),
 );
 ?>
